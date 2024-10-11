85b27f483b213b8935e14997f96010728ba83987
Harsh maurya <152727197+Harshmaury@users.noreply.github.com>
2024-10-12 04:46:50 +0530

    Add files via upload

---
+++ Java Script/Variable/01_basics/01_dataType.js
@@ -0,0 +1,28 @@
+"ues strict";  //treat  all JS code as newer version
+
+// allert (3 + 3)   we are using node.js not browser\
+
+console.log(3 + 3)
+
+console.log("harah")
+
+
+let name = "harsh"
+let age = 18
+let isLoggeID = false 
+
+// number => 2 to power 53
+// biging 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefind =>
+//symbol
+
+//object
+
+console.log(typeof "harsh")
+console.log(typeof null)
+console.log(typeof undefined)
+
+
